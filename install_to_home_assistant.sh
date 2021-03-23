750c293e19bda79563448a1439b83bc490ec22ff
Toni Korhonen <tokorhon@gmail.com>
2021-03-23 09:06:38 +0200

    Fixed issue with install script, if previous version was already installed

--- install_to_home_assistant.sh
+++ install_to_home_assistant.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Script that installs vallox2mqtt (latest from master) into Home Assistant
 git clone https://github.com/kotope/valloxesp.git
+rm -rf vallox2mqtt
 mv valloxesp/custom_components/vallox2mqtt .
 rm -rf valloxesp
